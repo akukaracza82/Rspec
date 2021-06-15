3cf19776e46c11b85a28cfc9246f4178e1fef316
Artur <amrozinski@gmail.com>
2021-06-15 11:58:11 +0100

    to the car

--- spec/integration/app/ledger_spec.rb
+++ spec/integration/app/ledger_spec.rb
@@ -1,10 +1,9 @@
-require_relative '../../../app/ledger'
+require_relative '../../../app/ledger.rb'
 require_relative '../../../config/sequel'
-require_relative '../../support/db'
 
 module ExpenseTracker
 
-  RSpec.describe Ledger, :aggregate_failures do
+  RSpec.describe Ledger, :aggregate_failures, :db do
     let(:ledger) { Ledger.new }
     let(:expense) do
       {
@@ -40,6 +39,22 @@ module ExpenseTracker
         expect(result.expense_id).to eq(nil)
         expect(result.error_message).to include('`payee` is required')
         expect(DB[:expenses].count).to eq(0)
+      end
+    end
+
+    describe '#expenses_on' do
+      it 'returns all expenses for the provided date' do
+        result_1 = ledger.record(expense.merge('date' => '2017-06-10'))
+        result_2 = ledger.record(expense.merge('date' => '2017-06-10'))
+        result_3 = ledger.record(expense.merge('date' => '2017-06-11'))
+        expect(ledger.expenses_on('2017-06-10')).to contain_exactly(
+          a_hash_including(id: result_1.expense_id),
+          a_hash_including(id: result_2.expense_id)
+        )
+      end
+
+      it 'returns a blank array when there are no matching expenses' do
+        expect(ledger.expenses_on('2017-06-10')).to eq([])
       end
     end
   end
